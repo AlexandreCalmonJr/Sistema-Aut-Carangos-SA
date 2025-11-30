ee225b0e7aa355afda28085a44b497f7596d7e50
AlexandreCalmon <alexandrecalmonjunior@gmail.com>
2025-11-30 11:38:47 -0300

    Ajuste template main

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, redirect, url_for, request, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
